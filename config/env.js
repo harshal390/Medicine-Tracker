b1e10f637f23a1fb377a8431cc5f3cf324d04d6b
Harshal Kahar <harshal.kahar@esparkbizmail.com>
2024-06-01 08:36:44 +0530

    📦 jsonwebtoken cookie-parser bcryptjs

--- config/env.js
+++ config/env.js
@@ -2,10 +2,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const config = {
-    port: process.env.PORT,
-    mysqlUserName: process.env.MYSQL_USERNAME,
-    mysqlPassword: process.env.MYSQL_PASSWORD,
-    projectDatabase: process.env.PROJECT_DATABASE,
+    port: parseInt(process.env.PORT),
+    mysqlUserName: String(process.env.MYSQL_USERNAME),
+    mysqlPassword: String(process.env.MYSQL_PASSWORD),
+    projectDatabase: String(process.env.PROJECT_DATABASE),
+    jwt_secret_key: String(process.env.JWT_SECRET_KEY),
+    jwt_expire: String(process.env.JWT_EXPIRE),
+    cookie_expire: parseInt(process.env.COOKIE_EXPIRE)
 };
 
 module.exports = config;
