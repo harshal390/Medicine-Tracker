90ca115c39b9eb0350dc4cf4ceeb1780c7c0642a
Harshal Kahar <harshal.kahar@esparkbizmail.com>
2024-06-05 10:47:21 +0530

    medication add in backend side complete with managed transaction

--- controllers/medication.controller.js
+++ controllers/medication.controller.js
@@ -1,10 +1,10 @@
 const { generalResponse } = require("../helpers/response.helper");
-const sequelize = require('sequelize');
 const { DateTimeToDate, DateTimeToTime } = require("../utils");
 const Medication = require('../models/index').sequelize.models.Medication;
 const OneTimeOnlyMedication = require('../models/index').sequelize.models.OneTimeOnlyMedication;
 const RecuringDaily = require('../models/index').sequelize.models.RecuringDaily;
 const RecuringWeekly = require('../models/index').sequelize.models.RecuringWeekly;
+const Transaction = require("../models/index").sequelize.transaction;
 
 const addMedication = async (req, res) => {
     try {
@@ -23,43 +23,48 @@ const addMedication = async (req, res) => {
             scheduleId: schedule,
             kindOfMedicationId: kindOfMedicationsMap[req.body.medicationType]
         }
-        const medication = await Medication.create({ ...MedicationData })
-        const medicationId = medication.id;
-        const ScheduleMap = {
-            1: [
-                OneTimeOnlyMedication,
-                {
-                    medicationId,
-                    date: DateTimeToDate(req.body.oneTimeOnlyDateTime),
-                    time: DateTimeToTime(req.body.oneTimeOnlyDateTime),
 
-                }
-            ],
-            2: [
-                RecuringDaily,
-                {
-                    medicationId,
-                    time: req.body.recuringDailytime,
-                    startDate: req.body.recuringDailyStartDate,
-                    endDate: req.body.recuringDailyEndDate
-                }
-            ],
-            3: [
-                RecuringWeekly,
-                {
-                    medicationId,
-                    day: parseInt(req.body.recuringWeeklySchedules),
-                    time: req.body.recuringWeeklyTime,
-                    startDate: req.body.recuringWeeklyStartDate,
-                    endDate: req.body.recuringWeeklyEndDate,
-                }
-            ],
-        }
-        // const createSchedule = await
-        console.log(medication);
-        generalResponse(res, medication, "add medication", "success", 1, 200);
+
+        await require("../models/index").sequelize.transaction(async (t) => {
+            const medication = await Medication.create({ ...MedicationData }, { transaction: t })
+            const medicationId = medication.id;
+            const ScheduleMap = {
+                1: [
+                    OneTimeOnlyMedication,
+                    {
+                        medicationId,
+                        date: req.body.oneTimeOnlyDateTime ? DateTimeToDate(req.body.oneTimeOnlyDateTime) : null,
+                        time: req.body.oneTimeOnlyDateTime ? DateTimeToTime(req.body.oneTimeOnlyDateTime) : null,
+
+                    }
+                ],
+                2: [
+                    RecuringDaily,
+                    {
+                        medicationId,
+                        time: req.body.recuringDailytime,
+                        startDate: req.body.recuringDailyStartDate,
+                        endDate: req.body.recuringDailyEndDate
+                    }
+                ],
+                3: [
+                    RecuringWeekly,
+                    {
+                        medicationId,
+                        day: parseInt(req.body.recuringWeeklySchedules),
+                        time: req.body.recuringWeeklyTime,
+                        startDate: req.body.recuringWeeklyStartDate,
+                        endDate: req.body.recuringWeeklyEndDate,
+                    }
+                ],
+            }
+            // console.log(ScheduleMap[schedule][0], { ...ScheduleMap[schedule][1] });
+            const createSchedule = await ScheduleMap[schedule][0].create({ ...ScheduleMap[schedule][1] }, { transaction: t })
+            generalResponse(res, createSchedule, "Medication Added successfully", "success", 1, 200);
+        })
     } catch (error) {
-        generalResponse(res, error.toString(), "add medication", "error", 1, 400);
+        console.log(error);
+        generalResponse(res, error.toString(), "Error occured while adding medication", "error", 1, 200);
     }
 }
 
